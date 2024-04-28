87350d33e4a80e1fc44e26139f1881f0fbfdfc49
Jacob Tweeten <jacobtweeten@Jacobs-Air.search.charter.net>
2024-04-28 18:41:49 -0400

    Setting up the database

--- ui/main.jsx
+++ ui/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 async function main() {
   const rootElt = document.getElementById("app");
   const root = createRoot(rootElt);
-  root.render(<h1>Hello From React woohoohoo !</h1>);
+  root.render(<h1>Hello From React Woohoo woohoohoo hoo hoo!</h1>);
 }
 
 window.onload = main;
